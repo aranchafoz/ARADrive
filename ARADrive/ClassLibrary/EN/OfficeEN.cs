a921bf80f0526bf2d73cfa02511ecf95c8256c04
Deathavenger <javier.renedo1@gmail.com>
2016-05-05 10:14:09 +0200

    Inclusion officeEN eliminar userEN
    N/A

---
+++ ARADrive/ClassLibrary/EN/OfficeEN.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace OfficeEnns
+{
+    public class OfficeEN
+    {
+
+        private int code;
+        private string name;
+        private string address;
+        private string city;
+        private float cX;
+        private float cY;
+
+
+        public OfficeEN(int code, string name, string address, string city, float cX, float cY)
+        {
+            this.code = code;
+            this.name = name;
+            this.address = address;
+            this.city = city;
+            this.cX = cX;
+            this.cY = cY;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public int CX
+        {
+            get { return cX; }
+            set { cX = value; }
+        }
+
+        public int CY
+        {
+            get { return cY; }
+            set { cY = value; }
+        }
+
+
+    }
+}
