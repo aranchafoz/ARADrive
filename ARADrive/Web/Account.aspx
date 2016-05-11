6de619edfddb72ff0ed92e906b047a232f3e1f20
aranchafoz <aranchunfli11foz@hotmail.com>
2016-05-11 04:08:51 +0200

    Design of My Account finished
    Profile page section

--- ARADrive/Web/Account.aspx
+++ ARADrive/Web/Account.aspx
@@ -1,11 +1,102 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ProfileNestedMasterPage.master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="Web.Formulario_web1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderProfile" runat="server">
-    <br />
-    <br />
-    <br />
-    <br />
-    <h2>My Account</h2>
-    <br />
-    <br />
-    <br />
+    
+    <div class="container-fluid">
+        <div class="row">
+            <asp:Label ID="Label_UserName" runat="server" Text="Mark" Font-Size="XX-Large"></asp:Label>
+            &nbsp;            
+            <asp:Label ID="Label_UserSurname" runat="server" Text="Sanders" Font-Size="XX-Large"></asp:Label>
+        </div>
+        <div class="row">
+            <div class="col-xs-6" style="margin:0;padding:0">
+                <asp:Label ID="Label_UserEmail" runat="server" Text="marksanders82@gmail.com" Font-Size="Large" Font-Italic="true"></asp:Label>         
+            </div>   
+            <div class="col-xs-6" style="text-align:right">
+                <asp:Label ID="Label_UserPremium" runat="server" Text="Regular User" Font-Size="Larger" Font-Bold="true" ForeColor="#996600"></asp:Label>
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon1"><img src="assets/img/Phone-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserPhone" class="form-control" runat="server" placeholder="Phone number" aria-describedby="basic-addon1"></asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/Phone-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserPhone" runat="server" Text="+34 622 10 41 88" Font-Size="Medium"></asp:Label>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon2"><img src="assets/img/Calendar-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserBirth" class="form-control" runat="server" placeholder="Birthdate" aria-describedby="basic-addon2"></asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/Calendar-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserBirth" runat="server" Text="27/04/1982" Font-Size="Medium"></asp:Label>
+                </div>
+            </div>     
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon3"><img src="assets/img/Building-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserCity" class="form-control" runat="server" placeholder="City" aria-describedby="basic-addon3"></asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/Building-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserCity" runat="server" Text="Aquapolis" Font-Size="Medium"></asp:Label>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon4"><img src="assets/img/Home-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserAddress" class="form-control" runat="server" placeholder="Address" aria-describedby="basic-addon4"></asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/Home-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserAddress" runat="server" Text="Av. Coldwater nº 23 7" Font-Size="Medium"></asp:Label>
+                </div>                
+            </div>     
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon5"><img src="assets/img/ID_Card-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserNIF" class="form-control" runat="server" placeholder="NIF" aria-describedby="basic-addon5"></asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/ID_Card-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserNIF" runat="server" Text="45692333S" Font-Size="Medium"></asp:Label>
+                </div>                
+            </div>
+            <div class="col-xs-6">
+                <!--<div class="input-group">
+                  <span class="input-group-addon" id="basic-addon6"><img src="assets/img/Driver_License_Card-20.png" /></span>
+                  <asp:TextBox ID="TextBox_UserDrivingLicense" class="form-control" runat="server" placeholder="Driving License" aria-describedby="basic-addon6">
+                  </asp:TextBox>
+                </div>-->
+                <div>
+                    <img src="assets/img/Driver_License_Card-20.png" />
+                    &nbsp;            
+                    <asp:Label ID="Label_UserDrivingLicense" runat="server" Text="None" Font-Size="Medium"></asp:Label>
+                </div>
+            </div>     
+        </div>
+        <br />
+        <div class="row" style="text-align:right">
+            <div class="col-xs-12">
+                <!--<asp:Button ID="Button_Save" class="btn btn-success" Width="20%" runat="server" Text="Save"/>-->
+                <asp:Button ID="Button_Edit" class="btn btn-info" Width="20%" runat="server" Text="Edit"/>
+            </div>  
+        </div>
+    </div>
 </asp:Content>
--- ARADrive/Web/Account.aspx.designer.cs
+++ ARADrive/Web/Account.aspx.designer.cs
@@ -11,5 +11,149 @@ namespace Web {
     
     
     public partial class Formulario_web1 {
+        
+        /// <summary>
+        /// Control Label_UserName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_UserName;
+        
+        /// <summary>
+        /// Control Label_UserSurname.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_UserSurname;
+        
+        /// <summary>
+        /// Control Label_UserEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_UserEmail;
+        
+        /// <summary>
+        /// Control Label_UserPremium.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_UserPremium;
+        
+        /// <summary>
+        /// Control TextBox_UserPhone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserPhone;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control TextBox_UserBirth.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserBirth;
+        
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Control TextBox_UserCity.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserCity;
+        
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Control TextBox_UserAddress.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserAddress;
+        
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Control TextBox_UserNIF.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserNIF;
+        
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Control TextBox_UserDrivingLicense.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_UserDrivingLicense;
+        
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
     }
 }
