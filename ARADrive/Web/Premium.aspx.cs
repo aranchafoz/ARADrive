bc7e017f1c627e44983e3e556dd7fae67d8d32b3
Deathavenger <javier.renedo1@gmail.com>
2016-05-17 01:30:39 +0200

    para josefa
    Version alpha de  premium aspx.cs y aspx

--- ARADrive/Web/Premium.aspx.cs
+++ ARADrive/Web/Premium.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClientCADNS;
+using ClientENns;
+using PaymentMethodENns;
+using PaymentMethodCADNS;
 
 namespace Web
 {
@@ -12,6 +16,29 @@ namespace Web
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Button_Submit_Click(object sender, EventArgs e)
+        {
+            string mail = (string)(Session[0]);
+            bool nuevoPremium = false;
+
+            ClientCAD clientCAD = new ClientCAD();
+            ClientEN client = clientCAD.getClient(mail);
+            PaymentMethodCAD paymentCAD = new PaymentMethodCAD();
+            PaymentMethodEN payment = paymentCAD.getPaymentMethod(mail);
+
+            if (client.Premium == true)
+            {
+                Button_PremiumUser.Text = "Already Premium";
+            }
+            else
+            {
+                if (payment.User != "user")
+                {
+                    client.Premium = true;
+                    nuevoPremium = clientCAD.updateCliente(client);
+                }
+            }
         }
     }
 }
