d0ac9941a7ec17c7b2faaf8330eaceadb0375dc6
Deathavenger <javier.renedo1@gmail.com>
2016-05-18 00:24:53 +0200

    funcionalidad premium
    premium aspx.cs

--- ARADrive/Web/Premium.aspx.cs
+++ ARADrive/Web/Premium.aspx.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClientCADNS;
@@ -10,34 +7,45 @@ using PaymentMethodENns;
 using PaymentMethodCADNS;
 
 namespace Web
+
 {
-    public partial class Premium : System.Web.UI.Page
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+        string mail = (string)(Session[0]);
+
+
+        ClientCAD clientCAD = new ClientCAD();
+        ClientEN client = clientCAD.getClient(mail);
+        PaymentMethodCAD paymentCAD = new PaymentMethodCAD();
+        PaymentMethodEN payment = paymentCAD.getPaymentMethod(mail);
+
+        if (client.Premium == true)
         {
+            Button_PremiumUser.Text = "Selected";
+            Button_PremiumUser.Enabled = false;
 
         }
-        protected void Button_Submit_Click(object sender, EventArgs e)
+    }
+    protected void Button_Submit_Click(object sender, EventArgs e)
+    {
+        string mail = (string)(Session[0]);
+
+
+        ClientCAD clientCAD = new ClientCAD();
+        ClientEN client = clientCAD.getClient(mail);
+        PaymentMethodCAD paymentCAD = new PaymentMethodCAD();
+        PaymentMethodEN payment = paymentCAD.getPaymentMethod(mail);
+
+        if (client.Premium == true)
         {
-            string mail = (string)(Session[0]);
-            bool nuevoPremium = false;
-
-            ClientCAD clientCAD = new ClientCAD();
-            ClientEN client = clientCAD.getClient(mail);
-            PaymentMethodCAD paymentCAD = new PaymentMethodCAD();
-            PaymentMethodEN payment = paymentCAD.getPaymentMethod(mail);
-
-            if (client.Premium == true)
+            Button_PremiumUser.Text = "Already Premium";
+        }
+        else
+        {
+            if (payment.User != "user")
             {
-                Button_PremiumUser.Text = "Already Premium";
-            }
-            else
-            {
-                if (payment.User != "user")
-                {
-                    client.Premium = true;
-                    nuevoPremium = clientCAD.updateClient(client);
-                }
+                client.Premium = true;
+                nuevoPremium = clientCAD.updateCliente(client);
             }
         }
     }
