1b112e00aa84efa9a7033060b2edf8f38f08e635
Martin Garcia Ripoll Muñoz (co-CEO) <martin@mainware.es>
2016-05-18 08:57:05 +0200

    Restored ClientCAD to connected
    ClientCAD is connected again.

--- ARADrive/ClassLibrary/CAD/ClientCAD.cs
+++ ARADrive/ClassLibrary/CAD/ClientCAD.cs
@@ -19,6 +19,7 @@ namespace ClientCADNS{
       private string s = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString ();
       private ClientEN client;
       private SqlConnection c;
+      private ArrayList allClients;
 
       public ClientCAD(){
         c = new SqlConnection(s);
@@ -27,100 +28,50 @@ namespace ClientCADNS{
 
 
 
-      public DataSet getAllClients(){
+      public ArrayList getAllClients(){
         try{
-          DataSet bdvirtual = new DataSet();
-          SqlDataAdapter sql = new SqlDataAdapter("SELECT * FROM T_User", c);
-          sql.Fill(bdvirtual,"user");
-          return bdvirtual;
+          c.Open();
+          allClients = new ArrayList();
+          SqlCommand com = new SqlCommand("Select * from T_User", c);
+          SqlDataReader dr = com.ExecuteReader();
+
+          while(dr.Read()){
+            ClientEN aux = new ClientEN(dr["email"], dr["pass"], dr["premium"], dr["DNI"], dr["name"], dr["surname"], dr["phone"], dr["address"], dr["city"], dr["birthDate"], dr["drivingLicence"]);
+            allClients.Add(aux);
+          }
+
+          dr.Close();
         }finally{
           c.Close();
+
+          return(allClients);
         }
       }
 
       public ClientEN getClient(String email){
-            ClientEN aux = null;
-            try {
-                DataSet bdvirtual = new DataSet();
-                SqlDataAdapter sql = new SqlDataAdapter("SELECT * FROM T_User WHERE email='"+email+"'", c);
-                sql.Fill(bdvirtual,"user");
-                DataTable t = new DataTable();
-                t = bdvirtual.Tables["user"];
-                DataRow dr = t.Rows[0];
-
-                int[] date = new int[3];
-                String birthDate = dr[9].ToString();
-                //String dLIss = dr[11].ToString();  // Añadidos datos de carnet
-                //String dLExp = dr[12].ToString();
-                /*int j = 0;
-                for (int i = 0; i < birthDate.Length; i++)
-                {
-                    String s = "";
-                    while (birthDate[i] != '-')
-                    {
-                        s += birthDate[i];
-                    }
-                    date[j] = int.Parse(s);
-                    j++;
-                }
-                Date bd = new Date(date[0], date[1], date[2]);*/
-
-                int day = Int32.Parse(birthDate.Substring(0, 2));
-                int month = Int32.Parse(birthDate.Substring(3, 2));
-                int year = Int32.Parse(birthDate.Substring(6, 4));
-                Date bd = new Date(day, month, year);
+        try{
+          c.Open();
+          SqlCommand com = new SqlCommand("Select * from T_User WHERE email='"+email+"'", c);
+          SqlDataReader dr = com.ExecuteReader();
 
 
-                /*int dayI = Int32.Parse(dLIss.Substring(0, 2));   // Añadidos datos de carnet
-                int monthI = Int32.Parse(dLIss.Substring(3, 2));
-                int yearI = Int32.Parse(dLIss.Substring(6, 4));
-                Date Iss = new Date(dayI, monthI, yearI);
+          ClientEN aux = new ClientEN(dr["email"], dr["pass"], dr["premium"], dr["DNI"], dr["name"], dr["surname"], dr["phone"], dr["address"], dr["city"], dr["birthDate"], dr["drivingLicence"]);
 
-                int dayE = Int32.Parse(dLExp.Substring(0, 2));   // Añadidos datos de carnet
-                int monthE = Int32.Parse(dLExp.Substring(3, 2));
-                int yearE = Int32.Parse(dLExp.Substring(6, 4));
-                Date Exp = new Date(dayE, monthE, yearE);*/
 
-                aux = new ClientEN(dr[0].ToString(), dr[1].ToString(), (bool)dr[2], dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), (int)dr[6], dr[7].ToString(), dr[8].ToString(), bd, (bool)dr[10]);  // Añadidos datos de carnet
-            }
-            catch (Exception ex)
-            {
-                throw new ArgumentException("Could not find a User for this Email-Adress");
-            }
-            finally {
-              c.Close();
-            }
-            return aux;
+          dr.Close();
+        }finally{
+          c.Close();
+
+          return(aux);
+        }
       }
 
 
-      public bool updateClient(ClientEN cl){
+      public void updateClient(ClientEN cl){
         try{
-          DataSet bdvirtual = new DataSet();
-          SqlDataAdapter sql = new SqlDataAdapter("SELECT * FROM T_User WHERE email='"+cl.Email+"'", c);
-          sql.Fill(bdvirtual,"user");
-          DataTable t = new DataTable();
-          t = bdvirtual.Tables["user"];
-          DataRow nuevafila = t.Rows[0];
-          nuevafila[0] = cl.Email;
-          nuevafila[1] = cl.Pass;
-          nuevafila[2] = cl.Premium;
-          nuevafila[3] = cl.DNI;
-          nuevafila[4] = cl.Name;
-          nuevafila[5] = cl.Surname;
-          nuevafila[6] = cl.Phone;
-          nuevafila[7] = cl.Address;
-          nuevafila[8] = cl.City;
-          nuevafila[9] = cl.BirthDate;
-          nuevafila[10] = cl.DrivingLicence;
-          //nuevafila[11] = cl.DLIss;        // Añadidos datos de carnet
-          //nuevafila[12] = cl.DLExp;
-          t.Rows.Add(nuevafila);
-          SqlCommandBuilder cbuilder = new SqlCommandBuilder(sql);
-          sql.Update(bdvirtual, "user");
-          return true;
-        }catch (Exception e) {
-            return false;
+          c.Open();
+          SqlCommand com = new SqlCommand("UPDATE T_User set pass='"+cl.Pass+"', premium="+cl.Premium+", DNI='"+cl.DNI+"', name='"+cl.Name+"', surname='"+cl.Surname+"', phone="+cl.Phone+", address='"+cl.Address+"', city='"+cl.City+"', birthDate='"+cl.BirthDate+"', drivingLicence="+cl.DrivingLicence + " WHERE email='"+cl.Email, c);
+          com.ExecuteNonQuery();
         }finally{
           c.Close();
         }
@@ -128,99 +79,22 @@ namespace ClientCADNS{
 
       public void deleteClient(String email){
         try{
-          DataSet bdvirtual = new DataSet();
-          SqlDataAdapter sql = new SqlDataAdapter("SELECT * FROM T_User WHERE email='"+email+"'", c);
-          sql.Fill(bdvirtual,"user");
-          DataTable t = new DataTable();
-          t = bdvirtual.Tables["user"];
-          t.Rows[0].Delete();
-          SqlCommandBuilder cbuilder = new SqlCommandBuilder(sql);
-          sql.Update(bdvirtual, "user");
+          c.Open();
+          SqlCommand com = new SqlCommand("DELETE FROM T_User WHERE email='"+email+"'", c);
+          com.ExecuteNonQuery();
         }finally{
           c.Close();
         }
       }
 
-        public bool insertCliente(ClientEN cl)
-        {
-            try {
-                /*DataSet dataSet = new DataSet();
-                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM T_User", c);
-                dataAdapter.Fill(dataSet,"user");
-                DataTable t = new DataTable();
-                t = dataSet.Tables["user"];
-                DataRow nuevafila = t.NewRow();
-                nuevafila[0] = cl.Email;
-                nuevafila[1] = cl.Pass;
-                nuevafila[2] = cl.Premium;
-                nuevafila[3] = cl.DNI;
-                nuevafila[4] = cl.Name;
-                nuevafila[5] = cl.Surname;
-                nuevafila[6] = cl.Phone;
-                nuevafila[7] = cl.Address;
-                nuevafila[8] = cl.City;
-
-                DateTime birth = new DateTime(cl.BirthDate.GetYear(), cl.BirthDate.GetMonth(), cl.BirthDate.GetDay());
-                nuevafila[9] = birth;
-
-                nuevafila[10] = cl.DrivingLicence;
-                t.Rows.Add(nuevafila);
-                SqlCommandBuilder cbuilder = new SqlCommandBuilder(dataAdapter);
-                dataAdapter.Update(dataSet, "user"); */
-
-                DataTable dataTable = new DataTable ();
-                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM T_User", c);
-                dataAdapter.Fill(dataTable);
-                DataRow nuevafila = dataTable.NewRow();
-                nuevafila[0] = cl.Email;
-                nuevafila[1] = cl.Pass;
-                nuevafila[2] = cl.Premium;
-                nuevafila[3] = cl.DNI;
-                nuevafila[4] = cl.Name;
-                nuevafila[5] = cl.Surname;
-                nuevafila[6] = cl.Phone;
-                nuevafila[7] = cl.Address;
-                nuevafila[8] = cl.City;
-
-                DateTime birth = new DateTime(cl.BirthDate.GetYear(), cl.BirthDate.GetMonth(), cl.BirthDate.GetDay());
-                nuevafila[9] = birth;
-
-                nuevafila[10] = cl.DrivingLicence;
-                dataTable.Rows.Add(nuevafila);
-
-                /*DateTime Iss = new DateTime(cl.DLIss.GetYear(), cl.DLIss.GetMonth(), cl.DLIss.GetDay()); // Añadidos datos de carnet
-                nuevafila[11] = ISs;
-
-
-                DateTime Exp = new DateTime(cl.DLExp.GetYear(), cl.DLExp.GetMonth(), cl.DLExp.GetDay()); // Añadidos datos de carnet
-                nuevafila[12] = Exp;*/
-
-                SqlCommandBuilder cbuilder = new SqlCommandBuilder(dataAdapter);
-                dataAdapter.Update((DataTable)dataTable);
-
-                return true;
-
-            } catch (Exception ex) {
-                Console.WriteLine(ex.Message.ToString());
-                return false;
-            }finally{
-              c.Close();
-            }
-          }
-            /*try
-            {
-                c.Open();
-                SqlCommand sql = new SqlCommand("INSERT INTO T_User VALUES (" + cl.Email + ", '" + cl.Pass + "', " + cl.Premium + ", '" + cl.DNI + "', '" + cl.Name + "', " + cl.Surname + ", " + cl.Phone + ", " + cl.Address + ", " + cl.City + ", " + cl.BirthDate + ", " + cl.DrivingLicence + ", " + ")", c);
-                sql.ExecuteNonQuery();
-                return true;
-            } catch (Exception e)
-            {
-                return false;
-            }
-            finally
-            {
-                c.Close();
-            }*/
-
+      public void insertCliente(ClientEN cl){
+        try{
+          c.Open();
+          SqlCommand com = new SqlCommand("INSERT INTO T_User VALUES('"+cl.Email+"', '"+cl.Pass+"', "+cl.Premium+",'"+cl.DNI+"', '"+cl.Name+"', '"+cl.Surname+"', "+cl.Phone+", '"+cl.Address+"', '"+cl.City+"', '"+cl.BirthDate+"', "+cl.DrivingLicence+")", c);
+          com.ExecuteNonQuery();
+        }finally{
+          c.Close();
         }
-  }
+      }
+    }
+}
